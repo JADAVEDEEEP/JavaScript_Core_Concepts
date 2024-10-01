1959809ea4c5778e61353d821df63cab63c27637
JADAVEDEEEP <jadavdeep560@gmail.com>
2024-10-01 17:58:27 +0530

    Day1

---
+++ Page 2 1 To 10 part/JavascriptOverview.js
@@ -0,0 +1,6 @@
+// javascipt is high level object oriented multilevel programing language//
+// holds the multiple abstractions//
+//that used to built ordern day javascript using framewoks like reactjs,angularjs//
+//Javascript also used to build backend with node js and express js///
+// using react native it would be possible to mobile application.//
+//javascript Eschecma Script built the version 5 to 10 //
