5024cccd35ea729ed23d118d5e4b7ae7e17b6f48
JADAVEDEEEP <jadavdeep560@gmail.com>
2024-11-07 19:11:09 +0530

    Day21

---
+++ Section 12 1To 8/DatesAndTimes.js
@@ -0,0 +1,18 @@
+//Create dates 
+//this in built function of javascript that wwill create new date in withdrwal or deposit everytime 
+const now = new Date()
+console.log(now)
+
+
+
+//it will generate the defual date that accroding to the data we apass 
+console.log(new Date(2037,10,15,23,5))
+console.log(new Date(60*60*1000*24))
+
+const deep = new Date(20,19,20,40)
+console.log(deep)
+
+//it will generate fullyear accreoding to the days ands motnhs and yers 
+console.log(deep.getFullYear())
+console.log(deep.getDate())
+console.log(deep.getMonth())
---
+++ Section 12 1To 8/ReminderOperator.js
@@ -0,0 +1,9 @@
+//it returns the reminder of disvion operator 
+
+console.log(5%5)
+console.log(5/2)//5=2*2+1
+
+//finding the even and odd number with reminder
+
+console.log(2%6)//Even
+console.log(8/2)//even
